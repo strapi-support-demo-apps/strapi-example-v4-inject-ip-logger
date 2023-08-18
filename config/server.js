4a6724fa5dc4924e672c746880e8cc654fb9f246
Derrick Mehaffy <derrickmehaffy@gmail.com>
2023-08-18 13:29:00 -0700

    init

---
+++ config/server.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  // This is required for ctx.request.ips to properly read the x-forwarded-for header
+  proxy: true,
+});
